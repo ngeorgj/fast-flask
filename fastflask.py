7cf3cc6969129b3cc55195124f7be2feb115e4f5
Nivaldo Georg Junior <ngj.netrunner@gmail.com>
2020-05-02 14:42:57 -0300

    updates on main file

--- fastflask.py
+++ fastflask.py
@@ -1,13 +1,59 @@
-import os, time
+# ==================================================================================================================== #
+# FAST FLASK                                  This is an minified application creator for flask applications.
+# Flask Minified App Generator                It will allow you to gain time on your flask projects that uses
+# Please follow me on GitHub: @ngeorgj        The basic starting code, it does everything for you!
+# ==================================================================================================================== #
 
-# PROGRAM FOR CREATING FLASK ALL-FILES
+import os
+import time
+import sys
 
-root = os.getcwd()
+available_os = {
+    'AIX': 'aix',
+    'Linux': 'linux',
+    'Windows': 'win32',
+    'macOS': 'darwin'}
 
-# FILE INFORMATION [APP, MODELS, ETC]
 
-# [app.py] File
-basic_app = """ 
+class FastFlask:
+    """
+    FAST FLASK SCRIPT
+    Flask made easy (yes, more than it already is).
+
+    The script basically runs a bunch of programs to create all files and folders you need to a basic
+    Flask application to run smoothly.
+
+    It will create an project structure like this:
+
+    [root]
+    |     [templates]
+    |     |       [index]
+    |     |       |      index.html
+    |     |       |
+    |     |       [admin]
+    |     |       |      admin.html
+    |     |       |
+    |     |       base.html
+    |     |
+    |     [static]
+    |     |       [css]
+    |     |       |      custom.css
+    |     |       |
+    |     |       [js]
+    |     |       |      custom.js
+    |     |       |
+    |     app.py
+    |     models.py
+    |     database.db
+    |
+
+    It's under development so it's really minified right now [May 08, 2020] but it will get better soon!
+
+    Hope you like it!
+
+    """
+    # [app.py]
+    f_app = """
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
@@ -44,19 +90,37 @@ def admin():
 
 if __name__ == '__main__':
     app.run(debug=True)
-"""
+        """
 
-# [models.py] File
-basic_model = """ 
+    # [models.py]
+    f_models = """
 
-"""
+        """
 
-def base_html(filename):
-    os.system(f'fsutil file createnew {filename} 2000')
-    file = os.getcwd() + "\\" + filename
-    print(file)
-    f = open(file, 'a')
-    f.write("""
+    def __init__(self):
+        self.os = ""
+        self.root = ""
+        self.get_os()
+
+    def get_os(self):
+        if sys.platform.startswith('aix'):
+            self.os = available_os['AIX']
+        elif sys.platform.startswith('linux'):
+            self.os = available_os['Linux']
+        elif sys.platform.startswith('win32'):
+            self.os = available_os['Windows']
+        elif sys.platform.startswith('darwin'):
+            self.os = available_os['macOS']
+        self.root = os.getcwd()
+        print("My OS is: " + self.os)
+
+    # HTML Basics
+    def base_html(self, filename):
+        os.system(f'fsutil file createnew {filename} 2000')
+        file = os.getcwd() + "\\" + filename
+        print(file)
+        f = open(file, 'a')
+        f.write("""
 <!doctype html>
 <html lang="en">
   <head>
@@ -66,10 +130,10 @@ def base_html(filename):
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    
+
     <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ url_for('static', filename='css/custom.css') }}">
-    
+
     <!-- basic navbar [DELETE IF NOT USEFUL] -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
@@ -104,29 +168,29 @@ def base_html(filename):
         </div>
       </div>
     </nav>
-    
+
     <!-- Block Head -->
     {% block head %} 
     <!-- title tag is inside the 'block head' in child html's -->
     {% endblock %}
-    
+
   </head>
   <body>
     {% block body %} 
     <!-- title tag is inside the 'block head' in child html's -->
-    
-    
+
+
         {% block content %}
-        
+
         {% endblock %}
-    
+
     {% endblock %}
-    
-        
-        
+
+
+
     <div class="container">
     <!-- DELETE BELOW THIS -->
-    
+
         <br>
         <h1> If you can read this, you are up to go!</h1>
         <br>
@@ -135,7 +199,7 @@ def base_html(filename):
         <h4> If you like this, please follow me on github <a style="color: red; text-decoration: none;" href="https://github.com/ngeorgj">@ngeorgj</a></h4>
         <br>
         <h4> In case of bugs, compliments or comments email to <b>ngj.netrunner@gmail.com</b></h4>
-        
+
     <!-- END DELETE HERE -->
     </div>
 
@@ -148,15 +212,15 @@ def base_html(filename):
     <script src="{{ url_for('static', filename='js/custom.js') }}">
   </body>
 </html>    
-    """)
-    f.close()
-    print(f'[{filename}] CREATED.')
+        """)
+        f.close()
+        print(f'[{filename}] CREATED.')
 
-def html_inherits_base(filename):
-    os.system(f'fsutil file createnew {filename} 2000')
-    file = os.getcwd() + "\\" + filename
-    f = open(file, 'a')
-    f.write("""
+    def html_inherits_base(self, filename):
+        os.system(f'fsutil file createnew {filename} 2000')
+        file = os.getcwd() + "\\" + filename
+        f = open(file, 'a')
+        f.write("""
 {% extends './base.html' %}
 
 {% block head %}
@@ -165,108 +229,137 @@ def html_inherits_base(filename):
 
 {% block body %}
 {% endblock %}
-    """)
+        """)
 
-def line(what):
-    os.system(f'{what}')
+    # Command Line
+    def line(self, what):
+        os.system(f'{what}')
+
+    def create_folder(self, folder_name):
+        os.system(f'mkdir {folder_name}')
+        print(f'[{folder_name}/] CREATED.')
+
+    def go_to_folder(self, dir_name=""):
+        """
+        Leaving go_to_folder(empty) will lead to root dir
+
+        :param dir_name: directory you want to move
+        """
+        os.chdir(self.root + '\\' + dir_name)
+
+    # Go to Root Directory
+    def root_dir(self):
+        self.line('cd ' + self.root)
+
+    # File Creation
+    def new_file(self, name, size):
+        self.line(f'fsutil file createnew {name} {size}')
+
+    def create_py(self, filename, content):
+        self.new_file(filename, 5000)
+        file = os.getcwd() + "\\" + filename
+        f = open(file, 'a')
+        f.write(content)
+        f.close()
+
+    # Test Server
+    def test_server(self):
+        # ASKS USER IF IT WANTS TO TEST
+        os.system('cls')
+        print(" Flask Minified Project Created Successfully\n")
+        choice = input(' Do you want to run the server for testing purposes [y/n]? > ')
+
+        yes = ['yes', 'YES', 'Yes', 'y', 'ye']
+        no = ['no', 'NO', 'No', ]
+
+        if choice in yes:
+            print("     Starting Test Server... [if response is 'not found' press 'F5']")
+            time.sleep(1)
+            os.system('cls')
+            self.line('start http://127.0.0.1:5000')
+            self.line('app.py')
+
+        elif choice in no:
+            print("     Terminating setup. Now you can open the folder with your IDE and edit it!")
+            time.sleep(3)
+            pass
+
+        else:
+            pass
+
+    # Directory & File Creation
+    def create_root(self):
+        """
+        Application goes
+
+        [root]
+          +  [Templates]
+          +  [Static]
+          +  app.py
+          +  models.py
+          +  database.db
+
+        """
+        self.create_folder('templates')
+        self.create_folder('static')
+        self.new_file('database.db', 8000)
+        self.create_py('app.py', self.f_app)
+        self.create_py('models.py', self.f_models)
+
+    def create_templates(self):
+        """
+        Template Folder Files
+
+        [root]
+          +  [Templates]
+            +  [index]
+                        index.html
+            +  [admin]
+                        admin.html
+            +  base.html
+
+        """
+        self.go_to_folder('templates')
+        self.base_html('base.html')
+        self.create_folder('index')
+        self.create_folder('admin')
+        self.go_to_folder('templates/index')
+        self.html_inherits_base('index.html')
+        self.go_to_folder('templates')
+        self.go_to_folder('templates/admin')
+        self.html_inherits_base('admin.html')
+        self.go_to_folder()
+
+    def create_static(self):
+        """
+        Static Folder Files
+
+        [root]
+          +  [Static]
+            +  [css]
+                        custom.css
+            +  [js]
+                        custom.js
+
+        """
+        self.go_to_folder('static')
+        self.create_folder('css')
+        self.create_folder('js')
+        self.go_to_folder('static/css')
+        self.new_file('custom.css', 1000)
+        self.go_to_folder('static/js')
+        self.new_file('custom.js', 1000)
+        self.go_to_folder()  # Goes back to Root
 
 
-def new_folder(folder_name):
-    os.system(f'mkdir {folder_name}')
-    print(f'[{folder_name}/] CREATED.')
+# Running FastFlask()
 
-def change_directory(dir_name=""):
-    os.chdir(root + '\\' + dir_name)
+fastflask = FastFlask()
 
+fastflask.create_root()
+fastflask.create_templates()
+fastflask.create_static()
+fastflask.test_server()
 
-def go_root():
-    line('cd ' + root)
-
-
-def new_file(name, size):
-    line(f'fsutil file createnew {name} {size}')
-
-
-def create_py(filename, content):
-    new_file(filename, 5000)
-    file = os.getcwd() + "\\" + filename
-    f = open(file, 'a')
-    f.write(content)
-    f.close()
-    print(f'[{filename}] CREATED.')
-
-
-# Create Templates and Static Folders
-new_folder('templates')
-new_folder('static')
-
-# TEMPLATES
-change_directory('templates')
-base_html('base.html')
-new_folder('index')
-new_folder('admin')
-change_directory('templates/index')
-html_inherits_base('index.html')
-change_directory('templates')
-change_directory('templates/admin')
-html_inherits_base('admin.html')
-change_directory()
-print('TEMPLATES FOLDER [OK]')
-
-# STATIC
-change_directory('static')
-new_folder('css')
-new_folder('js')
-change_directory('static/css')
-new_file('custom.css', 1000)
-change_directory('static/js')
-new_file('custom.js', 1000)
-change_directory()  # Goes back to Root
-print('STATIC FOLDER [OK]')
-
-# CREATE DATABASE
-new_file('database.db', 8000)
-print('[database.db] CREATED.')
-
-# CREATE THE 'APP.PY' FILE
-create_py('app.py', basic_app)
-
-# CREATE THE 'MODELS.PY' FILE
-create_py('models.py', basic_model)
-
-print("""
-    YOUR FOLDER STRUCTURE IS THE FOLLOWING:
-    
-    MAIN_FOLDER/
-     | > templates/
-     |   > index/
-     |             index.html
-     |   > admin/
-     |             admin.html
-     |   base.html
-     | > static/
-     |   > css/
-     |             custom.css
-     |   > js/
-     |             custom.js
-     app.py
-     models.py
-     database.db
-""")
-
-# ASKS USER IF IT WANTS TO TEST
-choice = input('     START TEST SERVER [Y/N]? > ')
-
-if choice == 'Y' or 'y':
-    print("     Starting Test Server... [if response is 'not found' press 'F5']")
-    time.sleep(1)
-    line('start http://127.0.0.1:5000')
-    line('app.py')
-else:
-    print("     Terminating setup. Now you can open the folder with your IDE and edit it!")
-    time.sleep(3)
-    pass
-
-
-
+# End
 
